0641ecef31fa5b06fd2cfecc923a38b626aa8c1e
MrxMilzu <89538890+MrxMilzu@users.noreply.github.com>
2021-09-12 03:18:31 +0700

    Update MBF.V20.py

--- MBF.V20.py
+++ MBF.V20.py
@@ -1,2 +1,5 @@
-import marshal
-exec(marshal.loads(b'\xe3\x00\x00\x00\x>
+#Encrypted with Crypton
+#Created by OVERDOSIS
+import base64
+import zlib
+exec(zlib.decompress(base64.b16decode("789CED3D6B731CC7719F835F315C976BEFC8C33D81030909641D8003090107C2C0
